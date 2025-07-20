8e933bc122fb39a8e0cef6bd8feca305118fd9a5
Russ Biggs <russbiggs@gmail.com>
2025-07-20 07:52:22 -0600

    httr2 update

--- R/organizations.R
+++ R/organizations.R
@@ -18,28 +18,25 @@ gh_v3_url <- function() {
 #' gh_organizations(since = 42)
 #' }
 gh_organizations <- function(since = 1) {
-  url <- httr::modify_url(
-    gh_v3_url(),
-    path = "organizations",
-    query = list(since = since)
-    )
 
   token <- Sys.getenv("GITHUB_PAT")
 
   if (!nchar(token)) {
-    stop("No token provided! Set up the GITHUB_PAT environment variable please.")
+    stop("No token provided! Set up the GITHUB_PAT environment variable please.", call. = FALSE)
   }
 
-  response <- httr::RETRY(
-    "GET",
-    url,
-    httr::add_headers("Authorization" = paste("token", token))
-  )
+  req <- httr2::request(gh_v3_url()) |>
+    httr2::req_url_path("organizations") |>
+    httr2::req_url_query(since = since) |>
+    httr2::req_auth_bearer_token(token) |>
+    httr2::req_retry(is_transient = \(resp) resp_status(resp) %in% c(500, 502, 503))
 
-  httr::stop_for_status(response)
 
-  content <- httr::content(response)
+  response <- httr2::req_perform(req)
+
+  httr2::resp_check_status(response)
+
+  content <- httr2::resp_body_json(response)
 
   purrr::map_chr(content, "login")
-
 }
--- tests/testthat/test-organizations.R
+++ tests/testthat/test-organizations.R
@@ -1,8 +1,9 @@
 vcr::use_cassette("organizations", {
 test_that("gh_organizations works", {
 
-  testthat::expect_message(foo <- gh_organizations(), "Retrying")
+  foo <- gh_organizations()
 
   testthat::expect_type(foo, "character")
 })
 })
+read
